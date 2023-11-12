0476cdd5b51bbdbbcb2f23fb8f75bd1eaf2ebade
EmmanuelJessy <emmanuel.adj8@gmail.com>
2023-11-12 23:59:59 +0000

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -165,30 +165,32 @@
               <div class="timeline timeline-one-side">
                 <div class="timeline-block mb-3">
                   <span class="timeline-step">
-                    <i class="material-icons text-success text-gradient">notifications</i>
+                    <i class="material-icons text-success text-gradient">videocam</i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">$2400, Design changes</h6>
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Hélène Larivée</h6>
                     <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
                   </div>
                 </div>
                 <div class="timeline-block mb-3">
                   <span class="timeline-step">
-                    <i class="material-icons text-danger text-gradient">code</i>
+                    <i class="material-icons text-success text-gradient">videocam</i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">New order #1832412</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 11 PM</p>
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Hélène Larivée</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
                   </div>
                 </div>
                 <div class="timeline-block mb-3">
                   <span class="timeline-step">
-                    <i class="material-icons text-info text-gradient">shopping_cart</i>
+                    <i class="material-icons text-success text-gradient">videocam</i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">Server payments for April</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 9:34 PM</p>
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">Hélène Larivée</h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
                   </div>
+                </div>
+                
                 </div>
                
                 
