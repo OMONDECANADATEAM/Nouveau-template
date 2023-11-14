a0f7b6d564ab7833a6c4fd2bb9b5572491451e96
Abed-Niamien <konan.niamien@uvci.edu.ci>
2023-11-14 10:24:46 +0000

    Ajout authenthification et fusion avec le frondend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,16 +15,17 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
-Route::get('home',[HomeController::class, 'index'])->name('home');
-Route::get('Banque',[HomeController::class, 'Banque'])->name('Banque');
-Route::get('Consultation',[HomeController::class, 'Consultation'])->name('Consultation');
-Route::get('DossierClients',[HomeController::class, 'DossierClients'])->name('DossierClients');
-Route::get('DossierContacts',[HomeController::class, 'DossierContacts'])->name('DossierContacts');
-Route::get('OmondeTeam',[HomeController::class, 'OmondeTeam'])->name('OmondeTeam');
-Route::get('sign-up',[HomeController::class, 'sign-up'])->name('sign-up');
-Route::get('sign-in',[HomeController::class, 'sign-in'])->name('sign-in');
-Route::get('profile',[HomeController::class, 'profile'])->name('profile');
-Route::get('virtual-reality',[HomeController::class, 'virtual-reality'])->name('virtual-reality');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
