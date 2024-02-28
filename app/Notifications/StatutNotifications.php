3352fc6279df924426c5348e34166f23483e77f0
unamiatoii <pierrejaphetkonan@gmail.com>
2024-02-28 16:04:20 +0000

    MDF MAIL

--- app/Notifications/StatutNotifications.php
+++ app/Notifications/StatutNotifications.php
@@ -48,7 +48,7 @@ class StatutNotifications extends Notification
                     ->subject('Avancement dans votre procedure d\'immigration')
                     ->greeting('Bonjour,')
                     ->line('Nous avons le plaisir de vous annoncer l\'avancement dans votre procdure')
-                    ->line('En effet vous etes maintenant à l\'etape de '. " .$this->statutLabel. ")
+                    ->line('En effet vous etes maintenant à l\'etape de "' . $this->statutLabel . '"')
                     ->line('Vous pouvez contacter votre agent de suivi pour plus d\'information ,' )
                     ->salutation('Cordialement.');
     }
