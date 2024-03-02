127df9750ed5c3952d506e5753b06e08e5e6b4b6
unamiatoii <pierrejaphetkonan@gmail.com>
2024-03-02 15:06:08 +0000

    COrrection des filtres Dirrection

--- resources/views/Direction/Partials/TableClient.blade.php
+++ resources/views/Direction/Partials/TableClient.blade.php
@@ -1,12 +1,12 @@
 <div class="card my-4">
     <div class="card-header p-0 position-relative mt-n4 mx-3">
-        <div
-            class="bg-gradient-dark border-radius-lg pt-4 pb-2 d-flex align-items-center justify-content-between p-4 flex-wrap">
-            <div class="p-2 border-radius-lg w-100 w-md-40 bg-white">
+        <div class="bg-gradient-dark border-radius-lg pt-4 pb-2 d-flex align-items-center justify-content-between p-4">
+            <div class="p-2 border-radius-lg w-40 bg-white">
                 <input type="text" id="searchInput" class="form-control text-dark text-lg bg-transparent border-0 p-1"
                     placeholder="Rechercher...">
             </div>
-            <div class="p-2 d-flex align-items-center w-100 w-md-30 justify-content-around flex-wrap">
+
+            <div class="p-2 d-flex align-items-center w-40 justify-content-around flex-direction-row">
                 <div class="dropdown">
                     <button class="btn btn-secondary" type="button" id="dropdownTypeVisa" data-toggle="dropdown">
                         Type de visa
@@ -23,6 +23,7 @@
                         @endforeach
                     </div>
                 </div>
+
                 <div class="dropdown">
                     <button class="btn btn-secondary" type="button" id="dropdownConsultante" data-toggle="dropdown">
                         Consultante
@@ -40,6 +41,7 @@
                         @endforeach
                     </div>
                 </div>
+
                 <div class="dropdown">
                     <button class="btn btn-secondary" type="button" id="dropdownSuccursales" data-toggle="dropdown">
                         Pays
@@ -56,9 +58,10 @@
                         @endforeach
                     </div>
                 </div>
+
+
             </div>
         </div>
-
     </div>
     <div class="card-body px-0 pb-2">
         <div class="table-responsive p-0" style="max-height: 700px; overflow-y: auto;">
@@ -80,8 +83,13 @@
                         </th>
                         <th
                             class="col-md-3 text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">
+                            AGENT / SUCCURSALE
+                        </th>
+                        <th
+                            class="col-md-3 text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">
                             MONTANT VERSÉ
                         </th>
+
                         <th
                             class=" col-md-2 text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">
                             VOIR DOSSIER
@@ -112,8 +120,8 @@
                             <td class="align-middle text-center">
                                 <span class="text-md consultante-name">
                                     @if ($candidat->proceduresDemandees)
-                                        {{ $candidat->proceduresDemandees->consultante->nom ?? 'N / A' }}
-                                        {{ $candidat->proceduresDemandees->consultante->prenoms ?? '' }}
+                                        {{ $candidat->proceduresDemandees->consultante->nom ?? 'null' }}
+                                        {{ $candidat->proceduresDemandees->consultante->prenoms ?? 'null' }}
                                     @else
                                         N / A
                                     @endif
@@ -122,12 +130,20 @@
 
                             <td>
                                 <div class="d-flex justify-content-center  px-2">
+                                    <span class="p-2 text-md text-center">{{ $candidat->utilisateur->name }}
+                                        {{ $candidat->utilisateur->last_name }} /
+                                        {{ $candidat->utilisateur->succursale->label }} </span>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="d-flex justify-content-center  px-2">
                                     {{-- <span class="p-2 text-md text-center">{{ $candidat->utilisateur->name }} {{ $candidat->utilisateur->last_name }} / {{ $candidat->utilisateur->succursale->label }} </span> --}}
 
                                     <span class="p-2 text-md text-center">
-                                        {{ isset($candidat->entrees) ? number_format($candidat->entrees->sum('montant'), 0, ',', ' ') . ' FCFA ' : 'Aucun versement' }}
-                                    </span>
 
+                                        {{ isset($candidat->entrees) ? number_format($candidat->entrees->sum('montant'), 0, ',', ' ') . ' FCFA ' : 'Aucun versement' }}
+
+                                    </span>
                                 </div>
                             </td>
 
