4a94cda5d8a15d69c2910df9d192e23360c84711
unamiatoii <pierrejaphetkonan@gmail.com>
2024-03-12 18:55:00 +0000

    Correction

--- resources/views/Administratif/Partials/AjouterOuModifierFiche.blade.php
+++ resources/views/Administratif/Partials/AjouterOuModifierFiche.blade.php
@@ -662,6 +662,16 @@
                             onclick="$('#modifierContactForm{{ $candidat->id }}').submit()">Enregistrer les
                             modifications</button>
                     </div>
+                    @if($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 
             </div>
 
