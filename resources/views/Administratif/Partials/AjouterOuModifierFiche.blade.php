453c24ef45566d484ae3a61170ef4093b1b50068
unamiatoii <pierrejaphetkonan@gmail.com>
2024-03-12 19:20:45 +0000

    Correction

--- resources/views/Administratif/Partials/AjouterOuModifierFiche.blade.php
+++ resources/views/Administratif/Partials/AjouterOuModifierFiche.blade.php
@@ -663,14 +663,15 @@
                             modifications</button>
                     </div>
                     @if($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+                    <div class="alert alert-danger">
+                        <ul>
+                            @dump($error)
+                            @foreach($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
 
 
             </div>
