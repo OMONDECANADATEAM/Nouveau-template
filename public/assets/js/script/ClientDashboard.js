49644ce0e350debc89a7f5fe6dfdc548bc23ae36
unamiatoii <pierrejaphetkonan@gmail.com>
2024-03-19 12:22:03 +0000

    TEST JS

--- public/assets/js/script/ClientDashboard.js
+++ public/assets/js/script/ClientDashboard.js
@@ -41,56 +41,56 @@
                 });
             });
 
-            // $('.consultationForm').submit(function(e) {
-            //     e.preventDefault(); // Empêche la soumission du formulaire par défaut
+            $('.consultationForm').submit(function(e) {
+                e.preventDefault(); // Empêche la soumission du formulaire par défaut
                 
-            //     // Afficher la page de chargement
-            //     $('#loading').addClass('show');
+                // Afficher la page de chargement
+                $('#loading').addClass('show');
     
-            //     var form = $(this);
-            //     var formData = form.serialize(); // Sérialisez les données du formulaire
+                var form = $(this);
+                var formData = form.serialize(); // Sérialisez les données du formulaire
     
-            //     $.ajax({
-            //         headers: {
-            //             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            //         },
-            //         type: form.attr('method'), // Récupère la méthode du formulaire (POST)
-            //         url: form.attr('action'), // Récupère l'URL du formulaire
-            //         data: formData, // Les données à envoyer
-            //         dataType: 'json', // Attend une réponse JSON du serveur
-            //         success: function(response) {
-            //             // Succès de la requête AJAX - afficher la réponse (succès ou échec)
-            //             if (response.success) {
-            //                 // Afficher un message de succès
-            //                 alert('La soumission du formulaire est terminée.');
+                $.ajax({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    type: form.attr('method'), // Récupère la méthode du formulaire (POST)
+                    url: form.attr('action'), // Récupère l'URL du formulaire
+                    data: formData, // Les données à envoyer
+                    dataType: 'json', // Attend une réponse JSON du serveur
+                    success: function(response) {
+                        // Succès de la requête AJAX - afficher la réponse (succès ou échec)
+                        if (response.success) {
+                            // Afficher un message de succès
+                            alert('La soumission du formulaire est terminée.');
     
-            //                 // Fermer le modal
-            //                 var modalId = form.data('modal-id');
-            //                 $('#modifierContactModal' + modalId).modal('hide');
-            //                 window.location.reload();
+                            // Fermer le modal
+                            var modalId = form.data('modal-id');
+                            $('#modifierContactModal' + modalId).modal('hide');
+                            window.location.reload();
 
-            //             } else if (response.errors) {
-            //                 // Afficher les erreurs de validation
-            //                 var errorMessage = 'Erreurs de validation :\n';
-            //                 $.each(response.errors, function(field, error) {
-            //                     errorMessage += field + ': ' + error + '\n';
-            //                 });
-            //                 alert(errorMessage);
-            //             } else if (response.error) {
-            //                 // Afficher toute autre erreur
-            //                 alert(response.error);
-            //             }
-            //         },
-            //         error: function(xhr, status, error) {
-            //             // Erreur lors de la requête AJAX
-            //             alert('Une erreur s\'est produite lors de l\'envoi de la requête AJAX : ' + error + xhr );
-            //         },
-            //         complete: function() {
-            //             // La soumission du formulaire est terminée - masquer la page de chargement
-            //             $('#loading').removeClass('show');
-            //         }
-            //     });
-            // });
+                        } else if (response.errors) {
+                            // Afficher les erreurs de validation
+                            var errorMessage = 'Erreurs de validation :\n';
+                            $.each(response.errors, function(field, error) {
+                                errorMessage += field + ': ' + error + '\n';
+                            });
+                            alert(errorMessage);
+                        } else if (response.error) {
+                            // Afficher toute autre erreur
+                            alert(response.error);
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        // Erreur lors de la requête AJAX
+                        alert('Une erreur s\'est produite lors de l\'envoi de la requête AJAX : ' + error + xhr );
+                    },
+                    complete: function() {
+                        // La soumission du formulaire est terminée - masquer la page de chargement
+                        $('#loading').removeClass('show');
+                    }
+                });
+            });
 
             
         });
